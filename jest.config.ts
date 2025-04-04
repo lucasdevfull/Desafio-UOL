6c8c4a23cb1355c4b9ee7e2f564ca816373fd55d
lucasdevfull <lucassilva270805@gmail.com>
2025-04-04 19:46:15 -0300

    Adicionando arquivo de configurações para testes

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+import { JestConfigWithTsJest, pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
+
+const config: JestConfigWithTsJest = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    rootDir: 'src',
+    testRegex: '.*\\.spec\\.ts$',
+    transform: {
+        '^.+\\.(t|j)s$': 'ts-jest',
+    },
+    collectCoverageFrom: ['**/*.(t|j)s'],
+    coverageDirectory: '../coverage',
+    moduleFileExtensions: ['js', 'json', 'ts'],
+    //moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths)
+}
+
+
+
+export default config
